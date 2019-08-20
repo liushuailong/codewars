dd96fb0136d37b4c239e5e11d53c150fb2106745
Barry Liu <liushuailong001@hotmail.com>
2019-08-20 18:03:58 +0800

    "some skills of python update"

--- some skills of python.py
+++ some skills of python.py
@@ -28,10 +28,27 @@ schedule.every().monday.do(job)
 schedule.every().wednesday.at("13:15").do(job)
 schedule.every().minute.at(":17").do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(1)
+
+from time import sleep
 
 
+def progress(percent=0, width=30):
+    left = width * percent // 100
+    right = width - left
+    print("[", "#" * left, " " * right, "]", f" {percent:.0f}%", sep="", end="", flush=True)
 
 
+# for i in range(101):
+#     progress(i)
+#     sleep(0.1)
+
+
+import json
+my_mapping = {"a": 23, "b": 42, "c": 0xc0ffee}
+print(json.dumps(my_mapping, indent=4, sort_keys=True))
+import pprint
+my_mapping = [{"a": 23, "b": 42, "c": 0xc0ffee}, {"a": 231, "b": 42, "c": 0xc0ffee}]
+pprint.pprint(my_mapping, width=4)
