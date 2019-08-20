fa82f73c7f0f813f5563ab03846000e5ff60e04d
Barry Liu <liushuailong001@hotmail.com>
2019-08-20 16:41:06 +0800

    "some skills of python update"

--- some skills of python.py
+++ some skills of python.py
@@ -8,9 +8,30 @@ def esc(code=0):
     return f'[{code}m'
 
 
-print(esc('31;1;0') + 'Error:' + esc() + 'important')
+# print(esc('31;1;0') + 'Error:' + esc() + 'important')
 
 # 在python中使用定时器
+
+
+import schedule
+import time
+
+
+def job():
+    print("I`m working...")
+
+
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("16:39").do(job)
+schedule.every().monday.do(job)
+schedule.every().wednesday.at("13:15").do(job)
+schedule.every().minute.at(":17").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
 
 
 
